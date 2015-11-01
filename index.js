f85523bd796dd53674c83dcc100c27f90f855176
maraf <mara@neptuo.com>
2015-11-01 10:11:51 +0100

    Apps index.html NG.

---
+++ index.js
@@ -0,0 +1,40 @@
+// JavaScript source code
+
+function updateUrl(url) {
+    if ('history' in window) {
+        window.history.pushState(url, url, url);
+    } else {
+        window.location.hash = url;
+    }
+}
+
+var $pages;
+var $links;
+
+function navigateToPage(page) {
+    $this = $links.filter("[href='" + page + "']");
+    $links.removeClass("active");
+    $this.addClass("active")
+
+    $pages.hide();
+    $page = $pages.filter("[data-page='" + page + "']")
+    if ($page.length == 0)
+        $page = $pages.filter("[data-page='not-found']");
+
+    $page.show();
+    updateUrl("detail/" + page);
+}
+
+$(function () {
+
+    $pages = $("[data-page]");
+    $links = $(".menu-item").click(function (e) {
+        e.preventDefault();
+
+        $this = $(this);
+        navigateToPage($this.attr('href'));
+    });
+
+    navigateToPage('chromium-updater');
+
+});
